80939d9ed93c6343d5abb2e66985d36c6bf91916
艳东 冯 <woaifengyandong@outlook.com>
2019-12-30 15:52:22 +0800

    11

--- WebAppli/Controllers/WeatherForecastController.cs
+++ WebAppli/Controllers/WeatherForecastController.cs
@@ -35,5 +35,18 @@ namespace WebAppli.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> eee()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
