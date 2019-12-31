9a01fdda1373f5ac60a6c06edc281dfe0dbfc497
魏许东 <@DESKTOP-M5AU0QM>
2019-12-31 15:01:47 +0800

    11

---
+++ WebAppli/Models/Wxd.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppli.Models
+{
+    public class Wxd
+    {
+        public int id { get; set; }
+    }
+}
