fe285037a4f06528b43604585e35331a158a2a9b
2531661286@qq.com <2531661286@qq.com>
2019-12-31 10:45:59 +0800

    YB

---
+++ WebAppli/Models/DBhNEL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppli.Models
+{
+    public class DBhNEL
+    {
+        public int MyProperty { get; set; }
+    }
+}
